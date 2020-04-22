3289c87c611ec7882ee36cfcfda8c4a7ba020fab
Max Wolffe <mwolffe@mwolffe-mn4.linkedin.biz>
2020-04-21 19:36:00 -0700

    Connect Postgres DB

--- yarb/settings.py
+++ yarb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'yarb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'yarb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
