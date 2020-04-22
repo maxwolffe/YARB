0538352754793e0a7bd672aee2e86c179d5b9102
Max Wolffe <mwolffe@mwolffe-mn4.linkedin.biz>
2020-04-21 21:04:04 -0700

    Add created_at and modified_at fields to the MealPlan object

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class RecipeBook(models.Model):
@@ -25,6 +26,15 @@ class MealPlan(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     recipes = models.ManyToManyField(Recipe, symmetrical=True)
+    created_at = models.DateTimeField(editable=False)
+    modified_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created_at = timezone.now()
+        self.modified_at = timezone.now()
+        return super(MealPlan, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return self.title + " " + self.created_at
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import RecipeBook, MealPlan
+from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    recipes_books = RecipeBook.get(author=user.id)[:5]
+    context = {
+        'recipes_books': recipes_books,
+    }
+    return render(request, 'recipes/index.html', context)
