8ce7a28523329a6bd06916c12e2597b5e654046f
Max Wolffe <mwolffe@mwolffe-mn4.linkedin.biz>
2020-04-28 20:57:37 -0700

    Use generic views

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     # ex: /
-    path('', views.index, name='index'),
+    path('recipe_books', views.RecipeBookIndexView.as_view(), name='index'),
+    path('recipe_books/<int:pk>', views.RecipeBookDetailView.as_view(),
+         name='recipe_book_detail'),
+    path('recipes/<int:pk>', views.RecipeDetailView.as_view(), name='recipe_detail'),
     path('accounts/signup', views.signup, name='signup')
 ]
